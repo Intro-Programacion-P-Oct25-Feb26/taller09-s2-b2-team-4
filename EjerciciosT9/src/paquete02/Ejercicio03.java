4e4ce76711bc4358525a4fbc455e4659d65bd737
daangamarca2 <arturoangamarca120@gmail.com>
2025-12-11 12:25:10 -0500

    Ejercicio-03

--- EjerciciosT9/src/paquete02/Ejercicio03.java
+++ EjerciciosT9/src/paquete02/Ejercicio03.java
@@ -17,7 +17,26 @@ public class Ejercicio03 {
     public static void main(String[] args) {
         // TODO code application logic here
         int[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
-        
-    }
+        double suma = 0;
 
+        for (int i = 0; i < arreglo.length; i++) {
+            suma += arreglo[i];
+        }
+
+        double media = suma / arreglo.length;
+        int arriba = 0;
+        int debajo = 0;
+
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] > media) {
+                arriba = arriba + 1;
+            } else if (arreglo[i] < media) {
+                debajo = debajo + 1;
+            }
+        }
+
+        System.out.printf("Media Aritmetica: %.2f\nElementos arriba de la media"
+                + " %d\nElementos debajo de la media: %d\n", media,arriba,
+                debajo);
+    }
 }
