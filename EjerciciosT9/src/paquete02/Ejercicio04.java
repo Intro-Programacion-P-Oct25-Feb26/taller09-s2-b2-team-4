23acdf937ac10b9661801022648461824317dce3
danyberna2002@gmail.com <danyberna2002@gmail.com>
2025-12-11 12:30:04 -0500

    problema4

--- EjerciciosT9/src/paquete02/Ejercicio04.java
+++ EjerciciosT9/src/paquete02/Ejercicio04.java
@@ -15,13 +15,31 @@ public class Ejercicio04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String mensaje_final = "";
         
         double[] promedios = {10, 10, 9.1, 7, 6.1, 4, 8};
         String[] estudiantes = {"Kimberly Gonzalez", "Mark Hogan", 
             "Teresa Martinez", "Julia Johnson", "Mark Cook", "Jennifer Manning",
             "Juan Vasquez"};
         String[] promediosCualitativos = new String[7];
-        
-    }
+        for (int i = 0; i < promedios.length; i++) {
 
+            double nota = promedios[i];
+
+            if (nota >= 0 && nota <= 5.9) {
+                promediosCualitativos[i] = "Regular";
+            } else if (nota >= 6 && nota <= 8.9) {
+                promediosCualitativos[i] = "Bueno";
+            } else if (nota >= 9 && nota <= 10) {
+                promediosCualitativos[i] = "Sobresaliente";
+            } else {
+                promediosCualitativos[i] = "Valor inválido";
+            }
+        }
+
+        for (int i = 0; i < estudiantes.length; i++) {
+            System.out.printf("%s promedio: %.2f promedio cualitativo?: %s%n",
+                    estudiantes[i], promedios[i], promediosCualitativos[i]);
+        }
+    }
 }
