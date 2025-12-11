9ba46a5e117f87123b018ac75e6e6c6123ca8437
danyberna2002@gmail.com <danyberna2002@gmail.com>
2025-12-11 15:22:26 -0500

    problema7

--- EjerciciosT9/src/paquete02/Ejercicio07.java
+++ EjerciciosT9/src/paquete02/Ejercicio07.java
@@ -5,20 +5,32 @@
  */
 package paquete02;
 
+import java.util.Locale;
 import java.util.Scanner;
 
-/**
- *
- * @author reroes
- */
 public class Ejercicio07 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-      
 
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        double[] numeros = new double[10];
+        double suma = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.printf("Ingrese el valor para posicion %d\n ", i + 1);
+            numeros[i] = entrada.nextDouble();
+            suma += numeros[i];
+        }
+
+        double promedio = suma / numeros.length;
+
+        System.out.printf("\nPromedio: %.2f\n", promedio);
+        System.out.println("\nNúmero   Diferencia con el promedio:");
+
+        for (int i = 0; i < numeros.length; i++) {
+            double diferencia = promedio - numeros[i];
+            System.out.printf("  %.2f - %.2f = %.2f\n", numeros[i], promedio, diferencia);
+        }
     }
-
 }
